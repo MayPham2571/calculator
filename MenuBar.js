fc4a24ba2d3dc7fedf5f5f6dcac0b00546dfcb14
maipham2571@gmail.com <maipham2571@gmail.com>
2021-01-26 08:47:40 +0700

    Adding MenuBar

---
+++ MenuBar.js
@@ -0,0 +1,34 @@
+// function myFunction(x){
+//     changeIcon(x);
+//     dropDown(x);
+// }
+
+// function changeIcon(x) {
+//     x.classList.toggle("change");
+// }
+
+// function dropDown(x) {
+//     var x = document.getElementById("myTopnav");
+//     if (x.className === "topnav") {
+//       x.className += " responsive";
+//     } else {
+//       x.className = "topnav";
+//     }
+// }
+
+function openPage(pageName,elmnt,color) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].style.backgroundColor = "";
+    }
+    document.getElementById(pageName).style.display = "block";
+    elmnt.style.backgroundColor = color;
+}
+  
+  // Get the element with id="defaultOpen" and click on it
+  document.getElementById("defaultOpen").click();
